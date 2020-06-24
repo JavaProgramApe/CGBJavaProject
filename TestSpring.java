655c1037d1fcaa4068a3e47ab97643fa945bcd15
JavaProgramApe <32978144+JavaProgramApe@users.noreply.github.com>
2020-06-24 15:33:02 +0800

    Update TestSpring.java

--- TestSpring.java
+++ TestSpring.java
@@ -2,7 +2,7 @@ package com.test;
 public class TestSpring {
 	public static void main(String[] args) {
 		/**
-		 * 通过spring容器中创建Hello对象，并调用它的方法
+		 * 通过spring容器创建Hello对象，并调用它的方法
 		 */
 		//1.加载spring的核心配置文件
 		ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
